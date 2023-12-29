67eab7a6ae6889cf43750adab574f69ba6accd55
Justin Sexton <justinsexton.dev@gmail.com>
2023-12-29 00:11:14 -0600

    Updated fibonacci solution to make use of memoization.

--- src/leetcode/fibonacci.rs
+++ src/leetcode/fibonacci.rs
@@ -1,9 +1,19 @@
+use std::collections::HashMap;
+
 /// https://leetcode.com/problems/fibonacci-number
-fn fibonacci(input: u32) -> u32 {
-    match input {
+fn fibonacci(target: u32) -> u32 {
+    fibonacci_memoized(target, &mut HashMap::new())
+}
+
+fn fibonacci_memoized(target: u32, cache: &mut HashMap<u32, u32>) -> u32 {
+    match target {
         0 => 0,
         1 => 1,
-        n => fibonacci(n - 2) + fibonacci(n - 1),
+        n => cache.get(&n).copied().unwrap_or_else(|| {
+            let result = fibonacci_memoized(n - 1, cache) + fibonacci_memoized(n - 2, cache);
+            cache.insert(n, result);
+            result
+        })
     }
 }
 
@@ -18,6 +28,7 @@ mod tests {
     #[test_case(2, 1)]
     #[test_case(3, 2)]
     #[test_case(4, 3)]
+    #[test_case(25, 75_025)]
     fn fibonacci_tests(input: u32, expected: u32) {
         assert_eq!(expected, fibonacci(input))
     }
