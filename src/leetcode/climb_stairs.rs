19c941b0b30c5547e67db69de76cfa9787350b33
Justin Sexton <justinsexton.dev@gmail.com>
2024-01-18 03:29:15 -0600

    Refactored to make use of a more declarative approach.

--- src/leetcode/climb_stairs.rs
+++ src/leetcode/climb_stairs.rs
@@ -1,28 +1,21 @@
 pub fn climb_stairs(n: i32) -> i32 {
-    if n == 0 || n == 1 {
-        return 1;
-    }
-
-    let mut curr = 1;
-    let mut prev = 1;
-    for _ in 2..=n {
-        let temp = curr;
-        curr = curr + prev;
-        prev = temp;
-    }
-
-    curr
+    let result = (0..n).fold((1, 0), |(res, prev), _| (res + prev, res));
+    result.0
 }
 
 #[cfg(test)]
 mod tests {
     use test_case::test_case;
+
     use crate::leetcode::climb_stairs::climb_stairs;
 
     #[test_case(0, 1)]
     #[test_case(1, 1)]
     #[test_case(2, 2)]
     #[test_case(3, 3)]
+    #[test_case(4, 5)]
+    #[test_case(5, 8)]
+    #[test_case(6, 13)]
     #[test_case(10, 89)]
     fn should_produce(input: i32, expected: i32) {
         let actual = climb_stairs(input);
