0da3c118ebfbd75ea5fcbf438b95b03449f3587b
jusexton <justinsexton.dev@gmail.com>
2024-08-05 22:49:47 -0500

    Refactor: Move nested function too root.

--- src/leetcode/beautiful_number.rs
+++ src/leetcode/beautiful_number.rs
@@ -1,19 +1,20 @@
 fn next_beautiful_number(n: i32) -> i32 {
-    fn is_beautiful(mut n: usize) -> bool {
-        let mut counts = [0; 10];
-        while n > 0 {
-            counts[n % 10] += 1;
-            n /= 10;
-        }
-        (0..10).all(|idx| {
-            let count = counts[idx];
-            match count {
-                0 => true,
-                _ => count == idx,
-            }
-        })
-    }
     (n + 1..).find(|n| is_beautiful(*n as usize)).unwrap()
+}
+
+fn is_beautiful(mut n: usize) -> bool {
+    let mut counts = [0; 10];
+    while n > 0 {
+        counts[n % 10] += 1;
+        n /= 10;
+    }
+    (0..10).all(|idx| {
+        let count = counts[idx];
+        match count {
+            0 => true,
+            _ => count == idx,
+        }
+    })
 }
 
 #[cfg(test)]
