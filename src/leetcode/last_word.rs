26b595e1410d460a5ffcaf8f3fa9e25cb4ba17ab
jusexton <justinsexton.dev@gmail.com>
2024-08-26 18:11:44 -0500

    Updated solution to handle empty string.

--- src/leetcode/last_word.rs
+++ src/leetcode/last_word.rs
@@ -1,10 +1,17 @@
 pub fn length_of_last_word(s: String) -> i32 {
-    s.split_whitespace().last().unwrap().len() as i32
+    s.split_whitespace()
+        .last()
+        .map_or(0, |word| word.len() as i32)
 }
 
 #[cfg(test)]
 mod tests {
     use super::length_of_last_word;
+
+    #[test]
+    fn empty_string() {
+        assert_eq!(0, length_of_last_word("".to_string()))
+    }
 
     #[test]
     fn without_padded_space() {
@@ -15,7 +22,7 @@ mod tests {
     fn with_padded_space() {
         assert_eq!(
             4,
-            length_of_last_word("   fly me   to   the moon  ".to_string())
+            length_of_last_word("   fly me   to   the   moon  ".to_string())
         )
     }
 }
