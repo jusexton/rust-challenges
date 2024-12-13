ad2bd6b2e7785f1487492b5ef9165b7065dc6edf
jusexton <justinsexton.dev@gmail.com>
2024-12-13 00:39:04 -0600

    Added tests for take gifts solution.

--- src/leetcode/take_gifts.rs
+++ src/leetcode/take_gifts.rs
@@ -1,13 +1,24 @@
 use std::collections::BinaryHeap;
 
-pub fn pick_gifts(gifts: Vec<i32>, mut k: i32) -> i64 {
+pub fn pick_gifts(gifts: Vec<i32>, mut seconds: i32) -> i64 {
     let mut heap = BinaryHeap::from(gifts);
 
-    while k > 0 {
+    while seconds > 0 {
         let pile = heap.pop().unwrap();
         heap.push((pile as f64).sqrt().floor() as i32);
-        k -= 1;
+        seconds -= 1;
     }
 
     heap.iter().map(|&n| n as i64).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::pick_gifts;
+
+    #[test]
+    fn computes_left_over_gifts_after_k_seconds() {
+        assert_eq!(29, pick_gifts(vec![25, 64, 9, 4, 100], 4));
+        assert_eq!(4, pick_gifts(vec![1, 1, 1, 1], 4));
+    }
 }
