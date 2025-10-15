937fed3ef2dc7715c0a5ae2da90751088e736404
Rajas Paranjpe <paranjperajas@gmail.com>
2025-10-15 12:29:53 -0700

    Use solution with XOR to count bit changes needed

--- src/leetcode/number_of_bit_changes_to_make_two_integers_equal.rs
+++ src/leetcode/number_of_bit_changes_to_make_two_integers_equal.rs
@@ -1,20 +1,27 @@
+// pub fn min_changes(n: i32, k: i32) -> i32 {
+//     let mut changes = 0;
+//     // The constraint is
+//     // 1 <= n, k <= 10^6
+//     // So bit 20 is the highest bit we might change
+//     for i in 0..=20 {
+//         match (k & (1 << i) != 0, n & (1 << i) != 0) {
+//             (false, false) | (true, true) => {}
+//             (false, true) => {
+//                 changes += 1;
+//             }
+//             (true, false) => {
+//                 return -1;
+//             }
+//         }
+//     }
+//     changes
+// }
+
 pub fn min_changes(n: i32, k: i32) -> i32 {
-    let mut changes = 0;
-    // The constraint is
-    // 1 <= n, k <= 10^6
-    // So bit 20 is the highest bit we might change
-    for i in 0..=20 {
-        match (k & (1 << i) != 0, n & (1 << i) != 0) {
-            (false, false) | (true, true) => {}
-            (false, true) => {
-                changes += 1;
-            }
-            (true, false) => {
-                return -1;
-            }
-        }
+    match n & k == k {
+        true => (n ^ k).count_ones() as _,
+        false => -1,
     }
-    changes
 }
 
 #[cfg(test)]
